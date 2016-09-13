f6fc3e147b6a9b46628825c25471b6f2d93469c0
Jacinda <jacindaz@gmail.com>
2016-09-13 13:44:39 -0400

    Method to set up a connection to mongodb

--- lib/database.rb
+++ lib/database.rb
@@ -1,9 +1,19 @@
+require 'pry'
+
 require 'sequel'
+require 'mongo_mapper'
 
 module Database
-  def self.db
+  def self.postgres_db
     Sequel.postgres(
         {host: 'localhost', port: 5432, database: 'learnup_dev', username: nil, password: nil}
       ).extension :pg_array, :pg_hstore, :pg_json
   end
+
+  def self.mongo_mapper_db
+    MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+    MongoMapper.database = "learnup"
+
+  end
 end
+
