9271abb2aa2752cdefb243c52abfe1d624119def
Jacinda <jacindaz@gmail.com>
2016-09-13 13:45:43 -0400

    Setting up importer class for importing CSV to mongodb

---
+++ lib/importers/mongodb_importer.rb
@@ -0,0 +1,14 @@
+require 'pry'
+require 'active_model/serializers'
+require 'mongo_mapper'
+
+require_relative '../database'
+
+module Importers
+  class MongoDB
+
+    def blah
+    end
+  end
+end
+
