bac89c432f81d9c0ed9fe94ff513f8004f149e73
Jacinda <jacindaz@gmail.com>
2016-09-14 14:48:47 -0400

    CSV importer for mongo, using mongo_mapper
     - why does mongoimport send everything to stderr ????
       so can't filter stderr and stdout, printing
       both just in case, so that you can tell if your
       import succeeded or not

--- lib/importers/mongodb_importer.rb
+++ lib/importers/mongodb_importer.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 require 'pry'
 require 'active_model/serializers'
 require 'mongo_mapper'
@@ -6,9 +8,23 @@ require_relative '../database'
 
 module Importers
   class MongoDB
+    def initialize(file_name, file_path)
+      @file_name = file_name
+      @file_path = file_path
+    end
 
-    def blah
+    def import_csv
+      Database.mongo_mapper_db # create a connection to the db
+
+      command = "mongoimport --db learnup --collection #{@file_name} --type csv --headerline --file #{@file_path}"
+
+      Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+        puts "\nSTDOUT\n" + stdout.read
+        puts "\nSTDERR\n" + stderr.read
+      end
     end
   end
 end
 
+m = Importers::MongoDB.new('client_one', '/Users/jacindazhong/Documents/jacinda/learnup_etl_version/data/client_one.csv')
+puts m.import_csv
