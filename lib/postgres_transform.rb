3d348feb8f5eaabcaeb40b0d116ffbc2dbe156cb
Jacinda <jacindaz@gmail.com>
2016-09-14 15:38:34 -0400

    Rename and add namespace for postgres transform file

--- lib/postgres_transform.rb
+++ lib/postgres_transform.rb
@@ -5,26 +5,28 @@ require 'pg'
 require_relative 'database'
 require_relative 'transform/users.rb'
 
-class Transform
-  include Database
+module Transform
+  class Postgres
+    include Database
 
-  def self.run
-    Transform.setup
-    Transform.run_transforms
-  end
+    def self.run
+      Transform.setup
+      Transform.run_transforms
+    end
 
-  def self.setup
-    Database.db.run <<-SQL
-      drop schema if exists applicant cascade;
-      create schema if not exists applicant;
-    SQL
-  end
+    def self.setup
+      Database.db.run <<-SQL
+        drop schema if exists applicant cascade;
+        create schema if not exists applicant;
+      SQL
+    end
 
-  def self.run_transforms
-    Users.run
-    # Jobs.run
-    # User::Jobs.run
-    # User::Statuses.run
+    def self.run_transforms
+      Users.run
+      # Jobs.run
+      # User::Jobs.run
+      # User::Statuses.run
+    end
   end
 end
 
