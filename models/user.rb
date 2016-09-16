ac621842e8d6556e96598e0febeef354a175f84c
Jacinda <jacindaz@gmail.com>
2016-09-16 14:58:13 -0400

    Modify validations, make email be unique

--- models/user.rb
+++ models/user.rb
@@ -9,17 +9,17 @@ module MongoModels
     include ::MongoMapper::Document
     set_collection_name "learnup_users"
 
-    key :email, String, length: { minimum: 4, maximum: 254, allow_nil: true }
+    key :email, String, length: { minimum: 4, maximum: 254, allow_nil: true }, unique: true
 
-    key :first_name, String
-    key :middle_name, String
-    key :last_name, String
+    key :first_name, String, allow_nil: true
+    key :middle_name, String, allow_nil: true
+    key :last_name, String, allow_nil: true
 
     validates_uniqueness_of :last_name, scope: [:first_name, :middle_name]
     validate :validate_full_name
 
-    key :prefix, String
-    key :suffix, String
+    key :prefix, String, allow_nil: true
+    key :suffix, String, allow_nil: true
 
     private
 
@@ -37,9 +37,9 @@ module MongoModels
 end
 
 
-email_not_valid = 'tasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfesdfasdfasdfasdfasdfsadfasdfsdfsadfsadfsadfsaasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfsadfasdfsdfasdfasdfasdfasaasdfasdfasdfsadfasdfsadfsadfasdfasdfasdfasdfasdfsadfasdfasdfasdfsadfsadfasdfasdfasdfdfdfst@test.com'
-valid_email = "test@test.com"
+# email_not_valid = 'tasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfesdfasdfasdfasdfasdfsadfasdfsdfsadfsadfsadfsaasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfsadfasdfsdfasdfasdfasdfasaasdfasdfasdfsadfasdfsadfsadfasdfasdfasdfasdfasdfsadfasdfasdfasdfsadfsadfasdfasdfasdfdfdfst@test.com'
+# valid_email = "test@test.com"
 
-a = MongoModels::User.new(first_name: 'Barnaby Marmaduke Aloysius Benjy Cobweb Dartagnan Egbert Felix Gaspar Humbert Ignatius Jayden Kasper Leroy Maximilian Neddy Obiajulu Pepin Quilliam Rosencrantz Sexton Teddy Upwood Vivatma Wayland Xylon Yardley Zachary Usansky', last_name: 'LastName', job: 'Cashier', status: 'Closed')
-Database.mongo_mapper_db
-a.save!
+# a = MongoModels::User.new(first_name: 'Barnaby Marmaduke Aloysius Benjy Cobweb Dartagnan Egbert Felix Gaspar Humbert Ignatius Jayden Kasper Leroy Maximilian Neddy Obiajulu Pepin Quilliam Rosencrantz Sexton Teddy Upwood Vivatma Wayland Xylon Yardley Zachary Usansky', last_name: 'LastName', job: 'Cashier', status: 'Closed')
+# Database.mongo_mapper_db
+# a.save!
